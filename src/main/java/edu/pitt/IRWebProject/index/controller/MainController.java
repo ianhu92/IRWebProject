e672dcfabca823659f43b2824d896447e08d5f4c
ianhu92 <ianhu92@gmail.com>
2015-12-10 10:03:45 -0500

    configure mvc with default servlet handler; add resource files

---
+++ src/main/java/edu/pitt/IRWebProject/index/controller/MainController.java
@@ -0,0 +1,35 @@
+package edu.pitt.IRWebProject.index.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+@RequestMapping("/")
+public class MainController {
+	String message = "Welcome to Spring MVC!";
+ 
+	/**
+	 * map index.html
+	 * @return
+	 */
+	@RequestMapping("index.html")
+	public String showIndex(){
+		return "index";
+	}
+	
+	/**
+	 * map the tested hello world page
+	 * @param name
+	 * @return
+	 */
+	@RequestMapping("hello.html")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -2,11 +2,15 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<link href="./resources/css/utils/initialization.css" type="text/css"
+	rel="stylesheet" />
+<script src="./resources/js/utils/jquery-1.11.3.min.js"></script>
 </head>
 <body>
 	<h2>Hello World</h2>
 	<h3>
 		<a href="./hello.html?name=World">Click Here</a>
 	</h3>
+
 </body>
 </html>
