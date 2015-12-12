696dfec166d8d2a34c1c0cca1d32ac97e9dadd6a
Siwei42 <siweiliu42@gmail.com>
2015-12-12 02:50:01 -0500

    Add ResultPage

---
+++ src/main/webapp/resources/js/ResultPage.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".limitURL").each(function(i){
+    len=$(this).text().length;
+    if(len>30)
+    {
+      $(this).text($(this).text().substr(0,30)+'...');
+    }
+  });       
+
+  $(".limitAnswer").each(function(i){
+    len=$(this).text().length;
+    if(len>500)
+    {
+      $(this).text($(this).text().substr(0,500)+'...');
+    }
+  });   
+});
+
+
+
